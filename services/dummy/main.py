9ef79b01dde99fc86e7d3cf1a75872f1a8389591
Matthias Zeller <m.zeller1901@gmail.com>
2024-03-11 14:43:33 +0100

    tgbot SSL

--- services/dummy/main.py
+++ services/dummy/main.py
@@ -11,11 +11,11 @@ config.setup(__file__)
 host = config.ServiceRegistry.get_service_hostname('tgbot')
 port = config.ServiceRegistry.SERVICE_TGBOT_PORT
 
-url = f'http://{host}:{port}/send_message'
+url = f'https://{host}:{port}/send_message'
 data = {'target': '2186', 'message': 'hello from dummy service!'}
 headers = {"Content-Type": "application/json"}
 
 
-response = requests.post(url, data=json.dumps(data), headers=headers)
+response = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
 print(response.text)
 
--- services/tgbot/main.py
+++ services/tgbot/main.py
@@ -6,5 +6,5 @@ from src.rest_api import *
 
 if __name__ == '__main__':
     setup(__file__)
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False, ssl_context='adhoc')
 
